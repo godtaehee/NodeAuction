d39b111debe13239cdfb53116c605a3ff643824d
taehee <wonderfulhuman@naver.com>
2021-10-04 17:10:59 +0900

    Add: Auction Init

---
+++ passport/index.js
@@ -0,0 +1,19 @@
+import passport from "passport";
+
+const local = require("./localStrategy");
+import User from "../models/user";
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      where: { id },
+    })
+      .then((user) => done(null, user))
+      .catch((err) => done(err));
+  });
+  local();
+};
